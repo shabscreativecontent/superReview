bcd724ce403aa31221564656036de8785f7b6c52
ShabsCreativeContent <ajideshedrack@gmail.com>
2023-06-05 18:33:13 -0700

    Review Update

--- objects-dog-photos/js/main.js
+++ objects-dog-photos/js/main.js
@@ -1,7 +1,7 @@
 //Get a dog photo from the dog.ceo api and place the photo in the DOM
-const url = "https://dog.ceo/api/breeds/image/random"
+const url = ""
 
-fetch().then(res => res.json()).then(data => {
+fetch(url).then(res => res.json()).then(data => {
   console.log(data);
 
   document.querySelector('img').src = data.message;
