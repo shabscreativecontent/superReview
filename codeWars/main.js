2e04fe13beb52fb00711a38f2676c21f1467f55f
ShabsCreativeContent <ajideshedrack@gmail.com>
2023-06-21 01:09:05 +0100

    super review

--- codeWars/main.js
+++ codeWars/main.js
@@ -136,5 +136,21 @@ function reTotalNum(arr){
   return arr.reduce((acc, c) => acc + Number(c), 0)
 }
 
-returnNum(arr)
-console.log(reTotalNum(arr));
+// returnNum(arr)
+// console.log(reTotalNum(arr));
+
+
+
+// Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+// solution('abc', 'bc') // returns true
+// solution('abc', 'd') // returns false
+
+
+
+function solution(str, ending){
+  // TODO: complete
+  return str.endsWith("bc") === str.endsWith(ending)
+}
+
+
+console.log(solution('abc', 'bc'));
