b558389ae0c0d6b99384d896d128e8e490fd4b26
ShabsCreativeContent <ajideshedrack@gmail.com>
2023-08-12 11:53:36 +0100

    code hurt

--- codeWars/main.js
+++ codeWars/main.js
@@ -258,3 +258,23 @@ console.log(pabs.walk());
 
 // arrayDiff([1,2,2,2,3],[2]) == [1,3]
 
+ 
+
+
+// Task
+// Given a list and a number, create a new list that contains each number of list at most N times, without reordering.
+// For example if the input number is 2, and the input list is [1,2,3,1,2,1,2,3], you take [1,2,3,1,2], drop the next [1,2] since this would lead to 1 and 2 being in the result 3 times, and then take 3, which leads to [1,2,3,1,2,3].
+// With list [20,37,20,21] and number 1, the result would be [20,37,21].
+
+function deleteNtn(arr){
+
+  const element = arr.filter((num) => num < 2)
+
+  // for (let i = 0; i < arr.length; i++) {
+  //   const element = arr[i]
+  // }
+
+  console.log(element);
+}
+
+deleteNtn([1,2,3,4,2,1,2,3,3])
