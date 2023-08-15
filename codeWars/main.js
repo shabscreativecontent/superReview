b63971f7d0e92520d204ced8532e4189fe092f2f
ShabsCreativeContent <ajideshedrack@gmail.com>
2023-08-15 16:29:21 +0100

    codeWar Update

--- codeWars/main.js
+++ codeWars/main.js
@@ -280,15 +280,34 @@ function deleteNtn(arr){
 deleteNtn([1,2,3,4,2,1,2,3,3])
 
 
+
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+// Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in. Additionally, if the number is negative, return 0 (for languages that do have them).
+
+// Note: If the number is a multiple of both 3 and 5, only count it once.
+
 let num = 20
 
 function mulOf3and5(n){
   if(n < 0){
     console.log(0)
+  }
+
+  for(let i = 1; i < n; i++){
+    if(i % 3 === 0 || i % 5 === 0){
+      console.log(i);
+    }
+  }
+}
+
+function mulsOf3and5(n){
+  if(n < 0){
+    console.log(0)
   }else {
     for(let i = 1; i < n; i++){
       if(i % 3 === 0 || i % 5 === 0){
-        console.log(i)
+        console.log(i);
       }
     }
   }
