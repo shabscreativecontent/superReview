64e5cd9fccc0a7627a647e3733f7072c3519f70a
ShabsCreativeContent <ajideshedrack@gmail.com>
2023-11-02 11:10:06 +0100

    Super Review Update

--- codeWars/main.js
+++ codeWars/main.js
@@ -336,3 +336,41 @@ function isPrime(num){
 }
 
 isPrime(5)
+
+
+
+
+// Create a function which answers the question "Are you playing banjo?".
+// If your name starts with the letter "R" or lower case "r", you are playing banjo!
+
+// The function takes a name as its only argument, and returns one of the following strings:
+
+
+function areYouPlayingBanjo(name){
+  if(name.startsWith('R') || name.startsWith('r')){
+    return `${name} plays banjo`
+  } else {
+    return `${name} does not play banjo`
+  }
+}
+
+console.log(areYouPlayingBanjo("rayo"))
+
+// // Similar code
+
+function areYouPlayingBanjo(name) {
+  return name + (name[0].toLowerCase() == 'r' ? ' plays' : ' does not play') + " banjo";
+}
+
+// // similar code
+
+function areYouPlayingBanjo(name) {
+  if (name[0].toLowerCase() === 'r') {
+    return name + ' plays banjo';
+  } else {
+    return name + ' does not play banjo';
+  }
+}
+
+
+
