ab121de805e308ba46e3753d7ba424262e9abef0
ShabsCreativeContent <ajideshedrack@gmail.com>
2023-11-06 11:50:42 +0100

    super review UpDated

--- codeWars/main.js
+++ codeWars/main.js
@@ -439,3 +439,22 @@ const maps1 = arr => arr.map( x => x * 2 )
 
 
 
+ // You were camping with your friends far away from home, but when it's time to go back, you realize that your fuel is running out and the nearest pump is 50 miles away! You know that on average, your car runs on about 25 miles per gallon. There are 2 gallons left.
+ 
+ // Considering these factors, write a function that tells you if it is possible to get to the pump or not.
+ 
+ // Function should return true if it is possible and false if not.
+ 
+ const zeroFuel = (distanceToPump, mpg, gallonsLeft) => {
+  const journyG = distanceToPump / mpg;
+  if(journyG <= gallonsLeft){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(zeroFuel(100,70,1))
+
+// // Similar code 
+const zeroFuel1 = (distanceToPump, mpg, fuelLeft) => mpg * fuelLeft >= distanceToPump;
