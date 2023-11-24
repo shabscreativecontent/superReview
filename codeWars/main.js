4a39fb6b6344fdd24a2ea8cd0532fe27d8bdda8d
ShabsCreativeContent <ajideshedrack@gmail.com>
2023-11-24 14:56:37 +0100

    Q

--- codeWars/main.js
+++ codeWars/main.js
@@ -536,3 +536,6 @@ function abbrevName(name){
   var nameArray = name.split(" ");
   return (nameArray[0][0] + "." + nameArray[1][0]).toUpperCase();
 }
+
+// You get an array of numbers, return the sum of all of the positives ones.
+
