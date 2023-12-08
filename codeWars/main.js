d6a5911bfb2e2de6659ceff833c6368fcbc552d1
ShabsCreativeContent <ajideshedrack@gmail.com>
2023-12-08 14:31:04 +0100

    Array

--- codeWars/main.js
+++ codeWars/main.js
@@ -572,3 +572,10 @@ function positiveSum(arr) {
   // return total
   return total;                         
 }
+
+
+// Array syntax 
+
+["Bilbo", "Gandalf", "Nazgul"].forEach((item, index, array) => {
+  // alert(`${item} is at index ${index} in ${array}`);
+});
