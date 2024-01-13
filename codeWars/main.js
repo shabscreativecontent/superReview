45a5f443f5ac64a9787269d777b43b226fc6a480
ShabsCreativeContent <ajideshedrack@gmail.com>
2024-01-13 23:48:23 +0100

    codewar

--- codeWars/main.js
+++ codeWars/main.js
@@ -644,3 +644,17 @@ function greet (name, owner) {
   // Add Code
   return name === owner ? 'Hello boss' : 	'Hello guest';
 }
+
+
+// Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+// For example, for [1, 2, 2] it should return 9
+
+const squareSum = function(arr){
+  let total = 0
+  arr.map(num => total += Math.pow(num, 2))
+  console.log(total)
+}
+
+// console.log()
+squareSum([2,2,2])
