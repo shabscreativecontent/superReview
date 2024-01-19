6b650ecdc36d50da0bb77af5032debf39d9e860d
ShabsCreativeContent <ajideshedrack@gmail.com>
2024-01-19 14:11:54 +0100

    yummy

--- codeWars/main.js
+++ codeWars/main.js
@@ -687,3 +687,5 @@ console.log(doubleInteger(3));
 
 // One:
 // Give a non-empty array of integers, return the result of multiplying the values together in other.
+// Example: [1,2,3,4] => 1*2*3*4 = 24
+
