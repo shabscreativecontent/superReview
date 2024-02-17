6be87cd531209ed491f5d16e1c84ac4c1214d226
ShabsCreativeContent <ajideshedrack@gmail.com>
2024-02-17 10:19:18 +0100

    logs

--- codeWars/main.js
+++ codeWars/main.js
@@ -820,3 +820,12 @@ console.log(
 
 const beasts = ['ant', 'bison', 'camel', 'duck', 'bison'];
 
+console.log(beasts.indexOf('bison'));
+// Expected output: 1
+
+// Start from index 2
+console.log(beasts.indexOf('bison', 2));
+// Expected output: 4
+
+console.log(beasts.indexOf('giraffe'));
+// Expected output: -1
