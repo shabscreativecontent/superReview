e1fbe2b0fab1469aa894f4026cca638c10032ed9
ShabsCreativeContent <ajideshedrack@gmail.com>
2024-02-27 22:34:00 +0100

    new war

--- codeWars/main.js
+++ codeWars/main.js
@@ -1291,3 +1291,23 @@ function factorial(num){
 
 console.log(factorial(5));
 
+
+/*
+Given an integer, return an integer that has the reverse ordering of that which was received. E.g reverseInteger(-123) // should return -321
+*/
+
+function reverseString(text){
+  return [...text].reduce((acc, char) => char + acc, '')
+}
+
+function reverseInteger(num){
+  let reverseNum = parseInt(reverseString(num.toString()))
+
+  return (reverseNum * Math.sign(num))
+}
+
+
+console.log(reverseInteger(-123));
+
+
+
