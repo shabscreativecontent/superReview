4597d71b4b608140d82733e0c60696bc32f8eb5e
ShabsCreativeContent <ajideshedrack@gmail.com>
2024-03-16 12:19:03 +0100

    Algorithm War

--- codeWars/main.js
+++ codeWars/main.js
@@ -1455,3 +1455,25 @@ function reverseStr(str){
 }
 
 console.log(reverseStr("shedrack"));
+
+// TASK: Your task is to write a function that takes a string and return a new string with all vowels removed.
+
+// For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+
+// Note: for this kata y isn't considered a vowel.
+
+
+function disemvowel(str) {
+  let vowels = ['a','e','i','o','u']
+  let result = ''
+
+  for(let i = 0; i < str.length; i++){
+    if (!vowels.includes(str[i].toLowerCase())) {
+      result += str[i]
+    }
+  }
+  
+  return result;
+}
+
+console.log(disemvowel("shedRAck love food"));
--- codeWars/new.js
+++ codeWars/new.js
@@ -1414,4 +1414,27 @@ function reverseStr(str){
   return newStr
 }
 
-console.log(reverseStr("shedrack"));
+console.log(reverseStr("shedrack love girls"));
+
+
+// TASK: Your task is to write a function that takes a string and return a new string with all vowels removed.
+
+// For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+
+// Note: for this kata y isn't considered a vowel.
+
+
+function disemvowel(str) {
+  let vowels = ['a','e','i','o','u']
+  let result = ''
+
+  for(let i = 0; i < str.length; i++){
+    if (!vowels.includes(str[i].toLowerCase())) {
+      result += str[i]
+    }
+  }
+  
+  return result;
+}
+
+console.log(disemvowel("shedRAck love food"));
